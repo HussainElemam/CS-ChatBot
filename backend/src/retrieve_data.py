5082ccc2fcb9cf47cb5e44d99832356251806eae
HussainElemam <130051441+HussainElemam@users.noreply.github.com>
2025-04-24 16:16:30 +0400

    summarize the history before sending it to the similarity search

--- backend/src/retrieve_data.py
+++ backend/src/retrieve_data.py
@@ -15,15 +15,34 @@ Answer the question based on the following context:
 
 Answer the question based on the above context: {question}
 """
+GETTING_CONTEXT_PROMPT = """
+Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:
+"""
 
 def query_rag(query_text, history):
+    model = Ollama(model=MODEL)
+
+    # summarize the conversation to get the context
+    prompt_template = ChatPromptTemplate.from_template(GETTING_CONTEXT_PROMPT)
+    summery_prompt_value = prompt_template.format_messages(chat_history=history, question=query_text)
+    summarized_conversation = model.invoke(summery_prompt_value)
+
+    print("\n\nsummarized conversation:\n", summarized_conversation, "\n\n\n")
+
     # Prepare the DB.
     embeddings = OllamaEmbeddings(model=MODEL)
     db = Chroma(persist_directory=VECTORSTORE_PATH, embedding_function=embeddings)
-    # Search the DB.
-    results = db.similarity_search_with_relevance_scores(query_text, k=5)
 
+    # Search the DB.
+    results = db.similarity_search_with_relevance_scores(summarized_conversation, k=5)
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    print("context text:\n", context_text)
+
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt_value = prompt_template.format_messages(context=context_text, question=query_text)
 
@@ -34,7 +53,6 @@ def query_rag(query_text, history):
     chat_history = get_history(history)
     chat_history.append(user_message)
 
-    model = Ollama(model=MODEL)
     response_text = model.invoke(chat_history)
 
     return response_text
